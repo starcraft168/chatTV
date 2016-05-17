415f26e6bd15bccf5dde1e82921e4bbaed93dc5e
starcraft168 <ericeng168@gmail.com>
2016-05-17 00:40:04 -0700

    get list of trusted urls as strings

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -5,9 +5,9 @@
 		.module('app.profile')
 		.controller('profileController', profileController);
 
-	profileController.$inject = ['$scope', 'profileService'];
+	profileController.$inject = ['$scope', '$sce', 'profileService'];
 
-	function profileController($scope, profileService) {
+	function profileController($scope, $sce, profileService) {
 		profileService.request().then(function(data) {
 			console.log('data', data.videos[0]._id);
 			console.log('title', data.videos[0].title)
@@ -18,14 +18,14 @@
 
 		$scope.show = show;
 		// $scope.getVideo = getVideo();
-		// $scope.getIframeSrc = getIframeSrc;
+		$scope.getIframeSrc = getIframeSrc;
 
 		function show() {
 			return 'hello';
 		}
 
 		function getIframeSrc(videoId) {
-			return "http://player.twitch.tv/?video=" + videoId;
+			return $sce.trustAsResourceUrl("http://player.twitch.tv/?video=" + videoId);
 		}
 
 		// function getVideo() {
